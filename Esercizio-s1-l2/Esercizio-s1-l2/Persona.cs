e23f9b313288a65ba603bc44d365be83a88761c1
DaveGio11 <145689622+DaveGio11@users.noreply.github.com>
2024-01-30 16:23:59 +0100

    Add files via upload

---
+++ Esercizio-s1-l2/Esercizio-s1-l2/Persona.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esercizio_s1_l2
+{
+    internal class Persona
+    {
+        public string Name {  get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+
+        public Persona(string Name, string Surname, int Age)
+        {
+            Name = Name;
+            Surname = Surname;
+            Age = Age;
+        }
+
+        public void getName()
+        {
+            Console.WriteLine("inserisci qui il tuo nome");
+            Name = Console.ReadLine();
+        }
+
+        public void getSurname()
+        {
+            Console.WriteLine("inserisci qui il tuo cognome");
+            Surname = Console.ReadLine();
+        }
+
+        public void getAge()
+        {
+            Console.WriteLine("inserisci qui la tuà età");
+            Age = int.Parse(Console.ReadLine());
+        }
+
+        public void getGeneral()
+        {
+            Console.WriteLine($"Il tuo nome è {Name}, il tuo cognome è {Surname}, la tua età è {Age}");
+           
+        }
+    }
+}
---
+++ Esercizio-s1-l2/Esercizio-s1-l2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esercizio_s1_l2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona persona1 = new Persona("", "", 0);
+
+            persona1.getName();
+            persona1.getSurname();
+            persona1.getAge();
+            persona1.getGeneral();
+            Console.ReadLine();
+        }
+    }
+}
